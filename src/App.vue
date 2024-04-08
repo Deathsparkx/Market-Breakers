40cc69ee2a2fb21b924a351c3ca98bd96f39e5d6
DESKTOP-BMCIUP1\khewk <khew@techtics.io>
2024-04-08 11:34:47 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+import MainUI from './components/MainUI.vue';
+import myImage from './assets/BG.png';
+
+export default {
+  data() {
+    return {
+      myImage: myImage
+    };
+  }
+};
+</script>
+
+<template>
+  <div>
+    <img :src="myImage" alt="My Image">
+  </div>
+</template>
---
+++ src/components/MainUI.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="image-container">
+        <img :src="imageUrl" alt="My Image" class="left-aligned">
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+        imageUrl: '//assets/BG.png' // Relative path to your image
+        };
+    }
+};
+</script>
+
+<style>
+.image-container {
+  margin: 0; /* Remove margin */
+  padding: 0; /* Remove padding */
+  max-width: none; /* Remove max-width constraint */
+}
+
+.left-aligned {
+  display: block; /* Ensure the image behaves as a block element */
+  margin-right: auto; /* Align to the left by setting margin-right to auto */
+}
+</style>
